41abc421e43150f6a537b9c7ec10af9ee426dd1f
good-mistake <devillmk88@gmail.com>
2023-10-03 02:20:06 +0330

    practice

--- src/home.js
+++ src/home.js
@@ -1,1 +1,8 @@
 console.log("Hello world!");
+const clickOnMe = document.getElementById("click");
+clickOnMe.addEventListener("click", () => {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+});
